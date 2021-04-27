de7fee42510ca7bf34349f7012f5b67d65b3298c
kadams66 <kn.adams66@gmail.com>
2021-04-27 18:39:50 -0400

    first boy

---
+++ testScript.py
@@ -0,0 +1,17 @@
+import numpy as np
+import os
+import sys
+
+a = 3
+b = 20
+c = 1E7
+arr = np.zeros(a)
+arr += b
+arr2 = np.zeros([a, a])
+for i in range(a):
+    for j in range(a):
+        arrs[i, j] = (i + 1) * j
+
+print(a / b)
+print(arrs @ b)
+print(format(c, '%d'))
